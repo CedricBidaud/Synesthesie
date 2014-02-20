742206bc3d070e846f7f5688c07e83d2e83a3e65
Cedric Bidaud <cedric.bidaud89@gmail.com>
2014-02-20 16:59:26 +0100

    ajout des fichiers du nouveau CMake

---
+++ build/CMakeFiles/Synesthésie.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Synesthésie.dir/src/RepulsiveForce.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/ConstantForce.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/Force.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/imgui.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/Polygon.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/UniversalForce.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/ParticleManager.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/PolygonForce.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/StickyForce.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/renderer/WindowManager.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/renderer/ParticleRenderer2D.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/renderer/GLtools.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/imguiRenderGL.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/BrakeForce.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/main.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/Leapfrog.cpp.o"
+  "CMakeFiles/Synesthésie.dir/src/HookForce.cpp.o"
+  "Synesthésie.pdb"
+  "Synesthésie"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Synesthésie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
