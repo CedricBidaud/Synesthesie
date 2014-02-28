5073e35438b06010db7763777854b2220a706fd7
Cedric Bidaud <cedric.bidaud89@gmail.com>
2014-02-28 11:27:42 +0100

    nettoyage vao vbo framebuffers

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,11 @@
 
 #include <glm/glm.hpp>
 
+static const Uint32 WINDOW_WIDTH = 1024;
+static const Uint32 WINDOW_HEIGHT = 1024;
+static const Uint32 TEXTURE_WIDTH = 1024;
+static const Uint32 TEXTURE_HEIGHT = 1024;
+
 namespace imac3{
 	
 		glm::vec2 normale(const glm::vec2& A, const glm::vec2& B){
@@ -255,6 +260,73 @@ namespace imac3{
 				exit( EXIT_FAILURE );
 			}
 			
+		}
+		
+		void loadGeometry(GLuint * vao, GLuint * vbo, GLuint * framebuffer, GLuint * texColorBuffer){
+			glGenFramebuffers(1, framebuffer);
+			glGenTextures(1, texColorBuffer);
+			glGenVertexArrays(1, vao);
+			glGenBuffers(3, vbo);
+			
+			int quadTriangleCount = 2;
+			int quadTriangleList[] = {0,1,2, 2,1,3};
+			float quadVertices[] = 
+				{
+					-1.f,	1.f,
+					-1.f,	-1.f,
+					1.f,	1.f,
+					1.f,	-1.f
+				};
+			float quadUVs[] = 
+				{
+					0.f,	1.f,
+					0.f,	0.f,
+					1.f,	1.f,
+					1.f,	0.f
+				};
+			
+			// renseignement du vao
+			glBindVertexArray(*vao);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[0]);
+					glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadTriangleList), quadTriangleList, GL_STATIC_DRAW);
+				
+				glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+					glEnableVertexAttribArray(0);
+					glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT)*2, (void*)0);
+					glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+					
+				glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+					glEnableVertexAttribArray(1);
+					glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT)*2, (void *)0);
+					glBufferData(GL_ARRAY_BUFFER, sizeof(quadUVs), quadUVs, GL_STATIC_DRAW);
+					
+				glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+			
+			// link du framebuffer et de la texture
+			glBindFramebuffer(GL_FRAMEBUFFER, *framebuffer);
+				glBindTexture(GL_TEXTURE_2D, *texColorBuffer);
+					glTexImage2D(
+						//~ GL_TEXTURE_2D, 0, GL_RGB, WINDOW_WIDTH, WINDOW_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL
+						GL_TEXTURE_2D, 0, GL_RGB, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL
+					);
+					
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+					
+					glFramebufferTexture2D(
+						GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, *texColorBuffer, 0
+					);
+				glBindTexture(GL_TEXTURE_2D, 0);
+				
+				GLenum compStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+				
+				if(compStatus == GL_FRAMEBUFFER_COMPLETE){
+					std::cout << "Framebuffer complet ! Code status : " << compStatus << std::endl;
+				}else{
+					std::cout << "Erreur : framebuffer incomplet - code status : " << compStatus << std::endl;
+				}
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		}
 		
 }
