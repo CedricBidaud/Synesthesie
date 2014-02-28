0229301c065ec222cc5ee69f43a2a2811627f814
Tom <matteo_fantasy@hotmail.fr>
2014-02-28 15:25:17 +0100

    Added Instrument files

---
+++ src/Instrument.cpp
@@ -0,0 +1,20 @@
+#include "Instrument.hpp"
+
+namespace imac3{
+
+	Instrument::Instrument(glm::vec2 position, glm::vec2 velocity, int index, ParticleManager* manager){
+		m_position = position;
+		m_velocity = velocity;
+		m_index = index;
+		m_manager = manager;
+	}
+
+	Instrument::~Instrument(){
+		
+	}
+
+	unsigned int Instrument::addParticle() {		
+		m_manager->addRandomParticle(m_position, m_velocity, m_index);
+	}
+
+} // end namespace imac3
---
+++ src/Instrument.hpp
@@ -0,0 +1,33 @@
+#ifndef _IMAC3_INSTRUMENT_HPP
+#define _IMAC3_INSTRUMENT_HPP
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <vector>
+#include <glm/gtc/random.hpp>
+
+#include <iostream>
+
+#include "ParticleManager.hpp"
+
+namespace imac3 {
+
+class Instrument{
+	public:
+		Instrument(glm::vec2 position, glm::vec2 velocity, int index, ParticleManager* manager);
+		~Instrument();
+		unsigned int addParticle();
+		
+		enum e_instrument { drums, bass, guitarA, guitarB };
+		
+	private:
+		glm::vec2 m_position;
+		glm::vec2 m_velocity;
+		int m_index;
+		ParticleManager* m_manager;
+		
+};
+
+}
+
+#endif
